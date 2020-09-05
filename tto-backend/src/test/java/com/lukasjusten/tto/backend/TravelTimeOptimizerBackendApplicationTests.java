002768503eefd9b6967f6fd65900be46da66f024
Lukas Justen <lukasjusten@gmail.com>
2020-09-05 12:09:35 +0200

    [TTO-001] Initialize TTO Spring Boot backend module

---
+++ tto-backend/src/test/java/com/lukasjusten/tto/backend/TravelTimeOptimizerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lukasjusten.tto.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelTimeOptimizerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
